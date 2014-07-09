865598f2ce3371516d90588d9737081c594b38b5
lmcdonough <levimcdonough@gmail.com>
2014-07-09 20:40:56 +0000

    added a sqlite db and ran firt syncdb

--- myproject1/settings.py
+++ myproject1/settings.py
@@ -1,4 +1,5 @@
 # Django settings for myproject1 project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -8,11 +9,12 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+BASE_DIR = os.getcwd()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
